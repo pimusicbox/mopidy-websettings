07ecb2ae4bf46c70e25e62e3d5d32e8d5959b291
woutervanwijk <wouter@iMac-3.local>
2014-09-21 22:32:59 +0200

    version 0.1.2

--- mopidy_websettings/__init__.py
+++ mopidy_websettings/__init__.py
@@ -11,7 +11,7 @@ from configobj import ConfigObj, ConfigObjError
 from validate import Validator
 import jinja2
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 logger = logging.getLogger(__name__)
 
