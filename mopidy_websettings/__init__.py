73b4b0b66f84e6d2d463f5f8a39dc8c9504377a4
woutervanwijk <wouter@iMac-3.local>
2014-11-24 22:19:07 +0100

    0.1.4 for pip, audioaddict

--- mopidy_websettings/__init__.py
+++ mopidy_websettings/__init__.py
@@ -11,7 +11,7 @@ from configobj import ConfigObj, ConfigObjError
 from validate import Validator
 import jinja2
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 logger = logging.getLogger(__name__)
 
